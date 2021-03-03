196fb744ee1ee60a5cfa743f200e27aaba888748
Adriana <am20dipi@alum.siena.edu>
2021-03-02 20:42:01 -0500

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get 'posts/:id', to: 'posts#show' 
+  #this draws a route that maps to a show action in the PostsController
+  # the ":id" says that this route can receive a parameter & that the parameter will be passed to the controller's show action.   
+
+
+
+  # Below: This is an abstract version of the code written above.
+  # resources :posts, only: :show 
 end
